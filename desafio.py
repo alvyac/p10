ce2285d4c1e3669e7f38bb8867578ac6ca60836d
almeidwx7 <alvaroalmeida185@gmail.com>
2026-04-15 17:03:18 -0300

    Add files via upload

---
+++ desafio.py
@@ -0,0 +1,7 @@
+lista = ["Cachorro", "Gato", "Peixe"]
+print(f"Lista de animais: {lista}")
+print(f"Primeiro animal: {lista[0]}")
+print(f"Último animal: {lista[-1]}")
+lista.append("Pato")
+print(f"Apos adicionar mais um animal: {lista}")
+print(f"Ultimo animal depois da adição: {lista[-1]}")
